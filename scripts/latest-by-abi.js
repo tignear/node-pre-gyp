383494a67f139492bcab8784bc83015ebbd044ea
iCrawl <icrawltogo@gmail.com>
2020-05-02 01:25:50 +0200

    chore: rewrite
    Bump lodash from 4.17.15 to 4.17.20 (#1)
    
    Bumps [lodash](https://github.com/lodash/lodash) from 4.17.15 to 4.17.20.
    - [Release notes](https://github.com/lodash/lodash/releases)
    - [Commits](https://github.com/lodash/lodash/compare/4.17.15...4.17.20)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- scripts/latest-by-abi.js
+++ scripts/latest-by-abi.js
@@ -5,24 +5,24 @@ const data = require('../lib/util/abi_crosswalk.json');
 
 const targets = {};
 Object.keys(data).forEach((v) => {
-  const o = data[v];
-  let abi;
-  if (o.node_abi === 1) {
-    abi = 'v8-' + o.v8;
-  } else {
-    abi = 'node-v' + o.node_abi;
-  }
-  if (targets[abi] === undefined) {
-    targets[abi] = v;
-  } else {
-    const cur = targets[abi];
-    if (semver.gt(v, cur)) {
-      targets[abi] = v;
-    }
-  }
+	const o = data[v];
+	let abi;
+	if (o.node_abi === 1) {
+		abi = `v8-${o.v8}`;
+	} else {
+		abi = `node-v${o.node_abi}`;
+	}
+	if (targets[abi] === undefined) {
+		targets[abi] = v;
+	} else {
+		const cur = targets[abi];
+		if (semver.gt(v, cur)) {
+			targets[abi] = v;
+		}
+	}
 });
 
 Object.keys(targets).forEach((k) => {
-  const version = targets[k];
-  console.log(version, data[version]);
+	const version = targets[k];
+	console.log(version, data[version]);
 });
